9f4fade76c29d1e744913570c79dbeda4c7ec337
Raul Germano <raulgermanorosendo@gmail.com>
2026-04-30 13:24:25 -0300

    Atualização 30/04/2026

---
+++ Demanda_22042026/BlinkTestagem.ino
@@ -0,0 +1,15 @@
+/*
+int led = 13; // pino do LED (padrão no Arduino Uno)
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH); // liga o LED
+  delay(280);              // espera 280 ms
+
+  digitalWrite(led, LOW);  // desliga o LED
+  delay(500);              // espera 500 ms
+}
+*/
